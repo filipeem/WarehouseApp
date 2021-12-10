8657a179f4671310c5952a1eb91ed73c85829617
filipeem <filipe.m@outlook.com>
2021-12-10 11:44:28 -0300

    cadastro de novos galpões

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -3,4 +3,17 @@ class WarehousesController < ApplicationController
     id = params[:id]
     @warehouse = Warehouse.find(id)
   end
+
+  def new
+  end
+
+  def create
+    warehouse_params = params.require(:warehouse).permit(:name, :code, :address, :state, :city,
+                                      :postal_code, :description, :useful_area,
+                                      :total_area )
+    warehouse = Warehouse.new(warehouse_params)
+    warehouse.save()
+    redirect_to warehouse_path(warehouse.id), notice: 'Galpão registrado com sucesso'
+  end
+  
 end
