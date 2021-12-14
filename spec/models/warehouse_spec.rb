c8e6f5bb642b147c0b3c9da1a6645d6ef062ae25
filipeem <filipe.m@outlook.com>
2021-12-14 11:50:03 -0300

    Insere testes unitários na criação de Galpão

--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -1,4 +1,206 @@
 require 'rails_helper'
 
 RSpec.describe Warehouse, type: :model do
+  it 'name é obrigatório' do
+    # Arrange
+    warehouse = Warehouse.new(name: '', code: 'MCZ', 
+                               description: 'Ótimo galpão mas é frio',
+                               address: 'Av Coritiba', city: 'Curitiba', state: 'PR',
+                               postal_code: '45000-000',
+                               total_area: 5000, useful_area: 4000)
+    # Act
+    result = warehouse.valid?
+
+    # Assert
+    expect(result).to eq false
+  end
+
+  it 'CEP é obrigatório' do
+    warehouse = Warehouse.new(name: 'Curitiba', code: 'MCZ', 
+                               description: 'Ótimo galpão mas é frio',
+                               address: 'Av Coritiba', city: 'Curitiba', state: 'PR',
+                               postal_code: '',
+                               total_area: 5000, useful_area: 4000)
+    # Act
+    result = warehouse.valid?
+
+    # Assert
+    expect(result).to eq false
+  end
+
+  it 'código é obrigatório' do
+    warehouse = Warehouse.new(name: 'Curitiba', code: '', 
+                               description: 'Ótimo galpão mas é frio',
+                               address: 'Av Coritiba', city: 'Curitiba', state: 'PR',
+                               postal_code: '45000-000',
+                               total_area: 5000, useful_area: 4000)
+    # Act
+    result = warehouse.valid?
+
+    # Assert
+    expect(result).to eq false
+  end
+
+  it 'descrição é obrigatório' do
+    warehouse = Warehouse.new(name: 'Curitiba', code: 'CWB', 
+                               description: '',
+                               address: 'Av Coritiba', city: 'Curitiba', state: 'PR',
+                               postal_code: '45000-000',
+                               total_area: 5000, useful_area: 4000)
+    # Act
+    result = warehouse.valid?
+
+    # Assert
+    expect(result).to eq false
+  end
+
+  it 'endereço é obrigatório' do
+    warehouse = Warehouse.new(name: 'Curitiba', code: 'CWB', 
+                               description: 'Ótimo galpão mas é frio',
+                               address: '', city: 'Curitiba', state: 'PR',
+                               postal_code: '45000-000',
+                               total_area: 5000, useful_area: 4000)
+    # Act
+    result = warehouse.valid?
+
+    # Assert
+    expect(result).to eq false
+  end
+
+  it 'cidade é obrigatório' do
+    warehouse = Warehouse.new(name: 'Curitiba', code: 'CWB', 
+                               description: 'Ótimo galpão mas é frio',
+                               address: 'Av Coritiba', city: '', state: 'PR',
+                               postal_code: '45000-000',
+                               total_area: 5000, useful_area: 4000)
+    # Act
+    result = warehouse.valid?
+
+    # Assert
+    expect(result).to eq false
+  end
+
+  it 'estado é obrigatório' do
+    warehouse = Warehouse.new(name: 'Curitiba', code: 'CWB', 
+                               description: 'Ótimo galpão mas é frio',
+                               address: 'Av Coritiba', city: 'Curitiba', state: '',
+                               postal_code: '45000-000',
+                               total_area: 5000, useful_area: 4000)
+    # Act
+    result = warehouse.valid?
+
+    # Assert
+    expect(result).to eq false
+  end
+
+  it 'área total é obrigatório' do
+    warehouse = Warehouse.new(name: 'Curitiba', code: 'CWB', 
+                               description: 'Ótimo galpão mas é frio',
+                               address: 'Av Coritiba', city: 'Curitiba', state: 'PR',
+                               postal_code: '45000-000',
+                               total_area:'', useful_area: 4000)
+    # Act
+    result = warehouse.valid?
+
+    # Assert
+    expect(result).to eq false
+  end
+
+  it 'área útil é obrigatório' do
+    warehouse = Warehouse.new(name: 'Curitiba', code: 'CWB', 
+                               description: 'Ótimo galpão mas é frio',
+                               address: 'Av Coritiba', city: 'Curitiba', state: 'PR',
+                               postal_code: '45000-000',
+                               total_area: 5000, useful_area: '')
+    # Act
+    result = warehouse.valid?
+
+    # Assert
+    expect(result).to eq false
+  end
+
+  it 'código não pode ser duplicado' do
+    # Arrange
+    warehouse = Warehouse.create(name: 'Maceió', code: 'MCZ', description: 'Ótimo galpão numa linda cidade',
+                                 address: 'Av Fernandes Lima', city: 'Maceió', state: 'AL',
+                                 postal_code: '57050-000',
+                                 total_area: 10000, useful_area: 8000)
+
+    warehouse2 = Warehouse.new(name: 'Curitiba', code: 'MCZ', 
+                               description: 'Ótimo galpão mas é frio',
+                               address: 'Av Coritiba', city: 'Curitiba', state: 'PR',
+                               postal_code: '87050-000',
+                               total_area: 5000, useful_area: 4000)
+    # Act
+    result = warehouse2.valid?
+
+    # Assert
+    expect(result).to eq false
+    
+  end
+
+  it 'nome não pode ser duplicado' do
+    # Arrange
+    warehouse = Warehouse.create(name: 'Maceió', code: 'MCZ', description: 'Ótimo galpão numa linda cidade',
+                                 address: 'Av Fernandes Lima', city: 'Maceió', state: 'AL',
+                                 postal_code: '57050-000',
+                                 total_area: 10000, useful_area: 8000)
+
+    warehouse2 = Warehouse.new(name: 'Maceió', code: 'CWB', 
+                               description: 'Ótimo galpão mas é frio',
+                               address: 'Av Coritiba', city: 'Curitiba', state: 'PR',
+                               postal_code: '87050-000',
+                               total_area: 5000, useful_area: 4000)
+    # Act
+    result = warehouse2.valid?
+
+    # Assert
+    expect(result).to eq false
+    
+  end
+
+  context 'cep inválido não pode ser registrado' do
+    it 'cep igual a 755' do
+      # Arrange
+      warehouse = Warehouse.new(name: 'Maceió', code: 'MCZ', description: 'Ótimo galpão numa linda cidade',
+                                  address: 'Av Fernandes Lima', city: 'Maceió', state: 'AL',
+                                  postal_code: '755',
+                                  total_area: 10000, useful_area: 8000)
+
+      # Act
+      result = warehouse.valid?
+
+      # Assert
+      expect(result).to eq false
+    end
+
+    it 'cep igual a 700000-00' do
+      # Arrange
+      warehouse = Warehouse.new(name: 'Maceió', code: 'MCZ', description: 'Ótimo galpão numa linda cidade',
+                                  address: 'Av Fernandes Lima', city: 'Maceió', state: 'AL',
+                                  postal_code: '700000-00',
+                                  total_area: 10000, useful_area: 8000)
+
+      # Act
+      result = warehouse.valid?
+
+      # Assert
+      expect(result).to eq false
+    end
+
+    it 'cep igual a aaaaa-aaa' do
+      # Arrange
+      warehouse = Warehouse.new(name: 'Maceió', code: 'MCZ', description: 'Ótimo galpão numa linda cidade',
+                                  address: 'Av Fernandes Lima', city: 'Maceió', state: 'AL',
+                                  postal_code: 'aaaaa-aaa',
+                                  total_area: 10000, useful_area: 8000)
+
+      # Act
+      result = warehouse.valid?
+
+      # Assert
+      expect(result).to eq false
+    end
+  end
+
 end
