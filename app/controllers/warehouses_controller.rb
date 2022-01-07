92032856553cb7d9837be03f5a970e6f3d58e37d
filipeem <filipe.m@outlook.com>
2022-01-07 09:18:20 -0300

    corrige rota da busca por Galpoes

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -43,5 +43,13 @@ class WarehousesController < ApplicationController
     end
    
   end
+
+  def search
+    @warehouses = Warehouse.where('name like ? OR city like ?',"%#{params[:q]}%", 
+                                "%#{params[:q]}%")
+    if @warehouse.nil?
+      flash.now[:alert] = "Termo #{params[:q]} não encontrado em Galpões"
+    end
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
-  resources :warehouses, only: [:show, :new, :create, :edit, :update]
+  resources :warehouses, only: [:show, :new, :create, :edit, :update] do
+    get 'search', on: :collection
+  end
   resources :suppliers, only: [:show, :new, :create, :index]
   resources :product_models, only: [:show, :new, :create, :edit, :update, :index]
   resources :product_bundles, only: [:show, :new, :create]
   resources :categories, only: [:show, :new, :create, :index]
   get 'product_items', to: 'product_items#new'
   post 'product_items', to: 'product_items#create'
-  get 'search', to:"home#search"
 
   
 end
